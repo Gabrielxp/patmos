558a733108f1a9f4419b790a396ab2625424e143
Jober <jobergc@gmail.com>
2017-04-30 17:29:44 -0300

    Programacao...

---
+++ app/src/main/java/com/example/gabri/patmos/Programacao_adapeter.java
@@ -0,0 +1,53 @@
+package com.example.gabri.patmos;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Jober on 30/04/2017.
+ */
+
+public class Programacao_adapeter extends BaseAdapter {
+    private final Context context;
+    private final List<Programas> programas;
+
+    public Programacao_adapeter(Context context, List<Programas> programas){
+        this.context = context;
+        this.programas = programas;
+    }
+
+    @Override
+    public int getCount() {
+        return programas != null ? programas.size() : 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return programas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View vi = LayoutInflater.from(context).inflate(R.layout.programacao_adapter,viewGroup,false);
+        TextView t = (TextView)view.findViewById(R.id.tNomePrograma);
+        ImageView img = (ImageView)view.findViewById(R.id.imgPrograma);
+
+        Programas p = programas.get(i);
+        t.setText(p.getNome());
+        img.setImageResource(p.img);
+
+        return vi;
+    }
+}
