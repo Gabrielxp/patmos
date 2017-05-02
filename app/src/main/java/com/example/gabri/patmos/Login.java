9bf25cb433ff2c38c0d642af690ab87695dded0b
Gabriel de Paula <gabriel.paula007@gmail.com>
2017-05-01 22:15:02 -0300

    Melhorias, principal e login

--- app/src/main/java/com/example/gabri/patmos/Login.java
+++ app/src/main/java/com/example/gabri/patmos/Login.java
@@ -19,6 +19,9 @@ public class Login extends AppCompatActivity {
         Button btProg = (Button) findViewById(R.id.btLogar);
         btProg.setOnClickListener(onClickBtProg());
 
+        Button btCad = (Button) findViewById(R.id.btCadastrar);
+        btCad.setOnClickListener(onClickBtCad());
+
     }
 
     private View.OnClickListener onClickBtProg() {
@@ -26,6 +29,19 @@ public class Login extends AppCompatActivity {
             public void onClick(View v){
                 //Navega para a prox tela
                 Intent intent = new Intent(Login.this,Principal.class);
+                //Bundle params = new Bundle();
+                //params.putString("nome","Jober Campos");
+                //intent.putExtras(params);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener onClickBtCad() {
+        return new Button.OnClickListener() {
+            public void onClick(View v){
+                //Navega para a prox tela
+                Intent intent = new Intent(Login.this,Cadastro.class);
                 //Bundle params = new Bundle();
                 //params.putString("nome","Jober Campos");
                 //intent.putExtras(params);
