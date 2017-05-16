e7d2bb5e6b51cde8ba727960d45b7aef646547c5
Jober <jobergc@gmail.com>
2017-05-15 22:24:38 -0300

    Shared Preferences Cadastro

--- app/src/main/java/com/example/gabri/patmos/Cadastro.java
+++ app/src/main/java/com/example/gabri/patmos/Cadastro.java
@@ -1,10 +1,16 @@
 package com.example.gabri.patmos;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,6 +20,9 @@ import java.util.List;
 
 public class Cadastro extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,8 @@ public class Cadastro extends AppCompatActivity {
         ActionBar bar = getSupportActionBar();
         bar.setDisplayHomeAsUpEnabled(true);
 
+        Button btCad = (Button) findViewById(R.id.btCadastrar);
+        btCad.setOnClickListener(onClickBtCad());
 
     }
 
@@ -35,4 +46,37 @@ public class Cadastro extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private View.OnClickListener onClickBtCad() {
+        return new Button.OnClickListener() {
+            public void onClick(View v){
+                TextView txtNome = (TextView) findViewById(R.id.tNome);
+                TextView txtEmail = (TextView) findViewById(R.id.tEmail);
+                TextView txtTel = (TextView) findViewById(R.id.tTelefone);
+
+                String nome = txtNome.getText().toString();
+                String email = txtEmail.getText().toString();
+                String tel = txtTel.getText().toString();
+
+                sharedPreferences = getSharedPreferences("PREF-CADASTRO", Context.MODE_PRIVATE);
+                editor = sharedPreferences.edit();
+                editor.putString("nome",nome);
+                editor.putString("email",email);
+                editor.putString("telefone",tel);
+
+                editor.apply();
+
+                sharedPreferences = getSharedPreferences("IS_LOGADO", Context.MODE_PRIVATE);
+                editor = sharedPreferences.edit();
+                editor.putString("email",email);
+                editor.apply();
+                
+                Intent intent = new Intent(Cadastro.this,MainActivity.class);
+
+                startActivity(intent);
+
+            }
+        };
+    }
+
 }
